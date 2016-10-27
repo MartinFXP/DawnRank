7a5816006566e27941d3e21e4d48de26b3a21583
MartinFXP <martin.pirkl@bsse.ethz.ch>
2016-10-27 10:27:00 +0200

    roygen2 update

--- R/condorcetRanking.R
+++ R/condorcetRanking.R
@@ -1,3 +1,60 @@
+#' Aggregate gene ranking over all patients
+#' 
+#' /code{condorcetRanking} Returns aggregate gene list over all patients.
+#' @param scoreMatrixa matrix containing all the given DawnRank scores per patient. Rows are genes, columns are  patients
+#' @param mutationMatrix the mutation matrix. Rows are genes, columns are patients
+#' @param pen the penality parameter in the condorcet algorithm for missing data. Default 0.85
+#' @return the ranks. A list of 2 including a [[1]] a matrix of all pairwise comparisons, [[2]] the final rankings based on the Condorcet score
+#' @examples
+#' ###using a small subset of the TCGA dataset and a small KEGG 
+#' ###gene interaction network,
+#' ###We will show how to get DawnRank Results
+#' 
+#' library(DawnRank)
+#' 
+#' #load the mutation data
+#' data(brcaExampleMutation)
+#' 
+#' #load the tumor expression data
+#' data(brcaExampleTumorExpression)
+#' 
+#' #load the normal expression data
+#' data(brcaExampleNormalExpression)
+#' 
+#' #load the pathway data
+#' data(brcaExamplePathway)
+#' 
+#' #load the gold standard
+#' data(goldStandard)
+#' 
+#' #normalize the tumor and normal data to get the differential expression
+#' normalizedDawn<-DawnNormalize(tumorMat=brcaExampleTumorExpression,
+#' normalMat=brcaExampleNormalExpression)
+#' 
+#' #get the DawnRank Score
+#' dawnRankScore<-DawnRank(adjMatrix=brcaExamplePathway,
+#' mutationMatrix=brcaExampleMutation,expressionMatrix=normalizedDawn, 
+#' mu=3,goldStandard=goldStandard)
+#' 
+#' #look at the DawnRank scores for a few patients
+#' dawnRankFrame<-dawnRankScore[[3]]
+#' head(dawnRankFrame)
+#' 
+#' #get the aggregate DawnRank scores
+#' aggregateDawnRankScore<-condorcetRanking(scoreMatrix=dawnRankScore[[2]],
+#' mutationMatrix=brcaExampleMutation)
+#' 
+#' #look at top 10 ranked genes
+#' top10<-aggregateDawnRankScore[[2]][1:10]
+#' top10
+#' 
+#' #get the individual cutoff for patient TCGA-A2-A04P
+#' dawnRankFrame$isCGC<-dawnRankFrame$isGoldStandard
+#' library(maxstat)
+#' significance<-patspeccutoff(patient="TCGA-A2-A04P",ms=dawnRankFrame,
+#' default=95)
+#' 
+#' @export
 condorcetRanking <- function (scoreMatrix, mutationMatrix, pen = 0.85, parallel = NULL, par = T, dovec = T) {
     pretrunc <- apply(mutationMatrix, 1, sum)
     trunc <- names(pretrunc[pretrunc > 0])
